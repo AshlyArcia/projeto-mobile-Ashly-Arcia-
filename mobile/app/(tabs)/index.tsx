2b1b2b6e4f6e5fd671ae0d6d849cec47dbdec6d5
AshlyArcia <ashly.arcia@escola.pr.gov.br>
2025-04-24 00:36:27 -0300

    Segundo commit

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -11,43 +11,34 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/itauindex.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Bem vindo Ashly!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Olá, Ashly!<HelloWave /></ThemedText>
+        
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+      <ThemedView style={styles.saldoContainer}>
+        <ThemedText type="subtitle">Bem vindo ao teu banco digital</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
+         CPF •••.123.456.-••
+        </ThemedText>
+
+      </ThemedView>
+      <ThemedView style={styles.infoContainer}>
+        <ThemedText type="subtitle">Aqui você pode...</ThemedText>
+        <ThemedText>
+        📊 Acompanhar teu saldo, fazer transferências e gerenciar teus cartões com segurança e praticidade.
         </ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+      <ThemedView style={styles.infoContainer}>
+        <ThemedText type="subtitle">O que ofrecemos é...</ThemedText>
         <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+        🚀 Um sistema rápido, simples e seguro.
+        O banco digital feito pra você ir além.{' '}
+        <ThemedText> Alem de, garantir a tua segurança em primeiro lugar. Tudo criptografado 
+        e protegido para você usar com tranquilidade.</ThemedText>{' '}
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -56,19 +47,21 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
   },
-  stepContainer: {
-    gap: 8,
+  saldoContainer: {
+    gap: 9,
     marginBottom: 8,
+    backgroundColor: '#FF4500',
+    borderRadius: 20,
+    alignItems: 'center',
+    padding: 5,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 278,
+    width: 450,
     bottom: 0,
-    left: 0,
     position: 'absolute',
   },
 });
